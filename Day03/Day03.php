4350d6bb712bcda91be8cfd134926b82a3c452f8
Aran Reeks <cdtreeks@gmail.com>
2019-12-03 11:03:15 +0000

    Initial run through for Day 3, parts 1 and 2

--- Day03/Day03.php
+++ Day03/Day03.php
@@ -8,28 +8,130 @@ require('../src/AdventOfCode.php');
 class Day03 extends AdventOfCode
 {
 
+    private array $currentPosition = [
+        'X' => 0,
+        'Y' => 0,
+    ];
+
     protected array $partOneTests = [
-        // TODO
+        'R75,D30,R83,U83,L12,D49,R71,U7,L72
+U62,R66,U55,R34,D71,R55,D58,R83' => 159,
+
+        'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+U98,R91,D20,R16,D67,R40,U7,R15,U6,R7' => 135,
     ];
 
     protected array $partTwoTests = [
-        // TODO
+        'R75,D30,R83,U83,L12,D49,R71,U7,L72
+U62,R66,U55,R34,D71,R55,D58,R83' => 610,
+
+        'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+U98,R91,D20,R16,D67,R40,U7,R15,U6,R7' => '410',
     ];
 
     /**
-     * @param int[] $input
+     * @param string[] $wires
+     *
+     * @return int
      */
-    public function getPartOne(array $input)
+    public function getPartOne(array $wires)
     {
-        // TODO
+        $grid = [];
+        $intersections = [];
+
+        foreach ($wires as $wire) {
+            $wirePath = [];
+            $this->currentPosition = ['X' => 0, 'Y' => 0]; // Reset
+
+            $directions = explode(',', $wire);
+
+            foreach ($directions as $position) {
+                $direction = $position[0];
+                $distance = substr($position, 1);
+
+                for ($i = 0; $i < $distance; $i++) {
+                    if ($direction === 'R') {
+                        $this->currentPosition['X'] += 1;
+                    } elseif ($direction === 'L') {
+                        $this->currentPosition['X'] -= 1;
+                    } elseif ($direction === 'U') {
+                        $this->currentPosition['Y'] += 1;
+                    } elseif ($direction === 'D') {
+                        $this->currentPosition['Y'] -= 1;
+                    }
+
+                    $currentCoordinate = $this->currentPosition['X'] . ',' . $this->currentPosition['Y'];
+
+                    if (isset($grid[$currentCoordinate]) && !isset($wirePath[$currentCoordinate]) && $currentCoordinate !== '0,0') {
+                        // Track intersections with other wires but not when at the starting position (0,0)
+                        $intersections[] = $currentCoordinate;
+                    }
+
+                    $grid[$currentCoordinate] = $wirePath[$currentCoordinate] = 'X';
+                }
+            }
+        }
+
+        $distancesFromCentralPort = [];
+        foreach ($intersections as $intersectionCoordinates) {
+            $distancesFromCentralPort[] = array_sum(array_map('abs', explode(',', $intersectionCoordinates)));
+        }
+
+        return min($distancesFromCentralPort);
     }
 
     /**
-     * @param int[] $input
+     * @param string[] $wires
+     *
+     * @return int
      */
-    public function getPartTwo(array $input)
+    public function getPartTwo(array $wires)
     {
-        // TODO
+        $grid = [];
+        $intersectionCoordinates = [];
+
+        foreach ($wires as $wireNumber => $wire) {
+            $wirePath = [];
+            $this->currentPosition = ['X' => 0, 'Y' => 0]; // Reset
+
+            $wireSteps = 0;
+
+            $directions = explode(',', $wire);
+
+            foreach ($directions as $position) {
+                $direction = $position[0];
+                $distance = substr($position, 1);
+
+                for ($i = 0; $i < $distance; $i++) {
+                    $wireSteps++;
+
+                    if ($direction === 'R') {
+                        $this->currentPosition['X'] += 1;
+                    } elseif ($direction === 'L') {
+                        $this->currentPosition['X'] -= 1;
+                    } elseif ($direction === 'U') {
+                        $this->currentPosition['Y'] += 1;
+                    } elseif ($direction === 'D') {
+                        $this->currentPosition['Y'] -= 1;
+                    }
+
+                    $currentCoordinate = $this->currentPosition['X'] . ',' . $this->currentPosition['Y'];
+
+                    if (isset($grid[$currentCoordinate]) && !isset($wirePath[$currentCoordinate]) && $currentCoordinate !== '0,0') {
+                        $intersectionCoordinates[] = $currentCoordinate;
+                    }
+
+                    $grid[$currentCoordinate][$wireNumber] = $wirePath[$currentCoordinate][$wireNumber] = $wireSteps;
+                }
+            }
+        }
+
+        $intersectionSteps = [];
+        foreach ($intersectionCoordinates as $intersectionCoordinate) {
+            $intersectionSteps[] = array_sum($grid[$intersectionCoordinate]);
+        }
+
+        return min($intersectionSteps);
     }
 }
 
--- Day03/README.md
+++ Day03/README.md
@@ -1,5 +1,10 @@
 ## Day 03 results 
 
 ```console
-Coming soon...
+Running tests...
+Part one test results: Passed 2/2 tests
+Part two test results: Passed 2/2 tests
+
+Part one: `232` (Completed in 0.30257s)
+Part two: `6084` (Completed in 0.42760s)
 ```
