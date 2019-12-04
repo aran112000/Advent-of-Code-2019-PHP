d47a3f514ceb35b8e11cb882d5dabafbb2274b93
Aran Reeks <cdtreeks@gmail.com>
2019-12-04 11:47:18 +0000

    Solutions for Day 04 parts 1 and 2

--- Day04/Day04.php
+++ Day04/Day04.php
@@ -8,28 +8,65 @@ require('../src/AdventOfCode.php');
 class Day04 extends AdventOfCode
 {
 
-    protected array $partOneTests = [
-        // TODO
-    ];
-
-    protected array $partTwoTests = [
-        // TODO
-    ];
+    const INPUT_DELIMITER = '-';
 
     /**
      * @param int[] $input
+     *
+     * @return int
      */
     public function getPartOne(array $input)
     {
-        // TODO
+        $possiblePasswords = [];
+
+        for ($i = $input[0]; $i < $input[1]; $i++) {
+            $code = str_pad($i, 6, '0', STR_PAD_LEFT);
+
+            // Check for 6 equal or ascending numbers
+            if (preg_match('/^(?=\d{6}$)0*1*2*3*4*5*6*7*8*9*$/', $code)) {
+
+                // Check for the presence of 1 or more adjacent repeating digits
+                if (preg_match('/(\d)\1+/', $code)) {
+                    $possiblePasswords[] = $code;
+                }
+            }
+        }
+
+        return count($possiblePasswords);
     }
 
     /**
+     * 365 = too low
+     *
      * @param int[] $input
+     *
+     * @return int
      */
     public function getPartTwo(array $input)
     {
-        // TODO
+        $possiblePasswords = [];
+
+        for ($i = $input[0]; $i < $input[1]; $i++) {
+            $code = str_pad($i, 6, '0', STR_PAD_LEFT);
+
+            // Check for 6 equal or ascending numbers
+            if (preg_match('/^(?=\d{6}$)0*1*2*3*4*5*6*7*8*9*$/', $code)) {
+
+                // Check for the presence of 1 or more adjacent repeating digits
+                $matches = [];
+                if (preg_match_all('/(\d)\1+/', $code, $matches)) {
+                    foreach ($matches[0] as $match) {
+                        if (strlen($match) === 2) {
+                            $possiblePasswords[] = $code;
+
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return count($possiblePasswords);
     }
 }
 
