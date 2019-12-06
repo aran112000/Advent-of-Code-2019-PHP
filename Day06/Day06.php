a3ab10b2aea085222f2a9185a1543b0fb8dc24f3
Aran Reeks <cdtreeks@gmail.com>
2019-12-06 11:36:44 +0000

    Day06, Part 1 solved

--- Day06/Day06.php
+++ Day06/Day06.php
@@ -11,7 +11,17 @@ class Day06 extends AdventOfCode
 {
 
     protected array $partOneTests = [
-        // TODO
+        'COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L' => 42,
     ];
 
     protected array $partTwoTests = [
@@ -19,18 +29,74 @@ class Day06 extends AdventOfCode
     ];
 
     /**
-     * @param int[] $input
+     * @param string[] $nodes
+     *
+     * @return int
      */
-    public function getPartOne(array $input)
+    public function getPartOne(array $nodes): int
+    {
+        $orbits = $this->getOrbits($nodes);
+
+        return $this->countOrbits($orbits);
+    }
+
+    /**
+     * @param string[] $nodes
+     */
+    public function getPartTwo(array $nodes)
     {
         // TODO
     }
 
     /**
-     * @param int[] $input
+     * Take the input data and produce a tree structure plotting each of the orbits
+     *
+     * @param string[] $nodes
+     *
+     * @return array
      */
-    public function getPartTwo(array $input)
+    protected function getOrbits(array $nodes): array
     {
-        // TODO
+        $orbits = [];
+        foreach ($nodes as $node) {
+            $nodeParts = explode(')', $node);
+            $orbits[$nodeParts[0]][$nodeParts[1]] = 'END';
+        }
+
+        $sortedOrbits = ['COM' => $orbits['COM']];
+        unset($orbits['COM']);
+
+        while ($orbits) {
+            array_walk_recursive($sortedOrbits, function (&$value, $key) use (&$orbits, $sortedOrbits) {
+                if (isset($orbits[$key])) {
+                    $value = $orbits[$key];
+                    unset($orbits[$key]);
+                }
+            });
+        }
+
+        return $sortedOrbits;
+    }
+
+    /**
+     * Count the total number of direct and indirect orbits in the provided data
+     *
+     * @param array $orbits
+     * @param int   $depth
+     *
+     * @return int
+     */
+    protected function countOrbits(array $orbits, int $depth = 0): int
+    {
+        $orbitCount = 0;
+        foreach ($orbits as $parent => $orbit) {
+            $orbitCount += $depth;
+
+            if (is_array($orbit)) {
+                $orbitCount += $this->countOrbits($orbit, $depth + 1);
+            }
+        }
+
+        return $orbitCount;
     }
 }
